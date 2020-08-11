920153d9ae68f70bd1fb1db21319e59cf1440581
imnotharsha <harshadodderi@gmail.com>
2020-08-11 20:56:23 +0530

    cors issue solved

---
+++ src/useFetchJobs.js
@@ -0,0 +1,46 @@
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+const ACTIONS = {
+  MAKE_REQUEST: 'make_request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    default:
+      return state;
+  }
+}
+//https://cors-anywhere.herokuapp.com/
+const BASE_URL =
+  'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, { params: { markdown: true, page: page, ...params } })
+      .then((res) =>
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+      )
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+  }, [params, page]);
+
+  return state;
+}
